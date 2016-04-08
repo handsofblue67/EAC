7aff5a835ffe9e5eede06cd8409e6afb44eb5484
Mark Ryan <markryandev@gmail.com>
2016-04-07 19:51:49 -0600

    Added Marks stuff

---
+++ CRAM_Classes/MyClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace YOUR_NAMESPACE
+{
+	abstract public class Alive
+	{
+		public int Attack { get; set; }
+		public int Defense { get; set; }
+		public Image MyImage { get; set; }
+	}
+
+	public class Monster : Alive
+	{
+
+	}
+
+	public class Hero : Alive
+
+	{
+		public int Health { get; set; }
+		public int Runs { get; set; }
+		public int KillCount { get; set; }
+	}
+}
+
